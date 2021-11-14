67a6eff20052eeaec0d4cb268fd997e03cd52a96
Khoa Phan <cun1806@gmail.com>
2021-11-14 06:32:43 -0800

    Updated description and analysis, added ROC curve

--- Vina Team Problem 2 Stark Inc.R
+++ Vina Team Problem 2 Stark Inc.R
@@ -17,7 +17,6 @@
 
 library(rpart)
 library(rpart.plot)
-#install.packages("fracdiff")
 library(forecast)
 library(caret)
 library(ISLR)
@@ -26,12 +25,16 @@ require(tree)
 library(tidyr)
 library(dplyr)
 
-# Load data ------------------------------------------------------------
+# Data import and transformation ---------------------------------------------
+# For the data, we decided to remove all variables that were deemed unecessary, turn all the remaining categorical variables into factors,
+# remove missing values, and change the output variable into something more human friendly (low-risk and high-risk).
+
+# Data importing
 credit <- read.csv("credit_2.csv", header = TRUE)
 head(credit, 10)
 names(credit)
 
-# Remove unnecessary vars
+# Removing all unnecessary variables
 credit_filtered <- credit[, -c(1,19,20,23,24,26,32,33,34:36,41,42:61,67)]
 credit_filtered <- credit_filtered [,-c(16,21,30:33)]
 credit_filtered <- credit_filtered [,-c(9,10,13,18,25,27,28)]
@@ -39,11 +42,13 @@ credit_filtered <- credit_filtered [,-c(1,10:14,17)]
 credit_filtered <- drop_na(credit_filtered)
 names(credit_filtered)
 
-# Change the dependent var into actual text
+# Changing the dependent variable into actual text
 credit_filtered$TARGET <- factor(credit_filtered$TARGET,
                                  levels = c("0", "1"),
                                  labels = c("Low-risk", "High-risk"))
-# Factorise all the categorical vars
+
+
+# Factorize all the categorical variables
 credit_filtered $TARGET <- as.factor(credit_filtered $TARGET)
 credit_filtered $NAME_CONTRACT_TYPE  <- as.factor(credit_filtered $NAME_CONTRACT_TYPE)
 credit_filtered $CODE_GENDER <- as.factor(credit_filtered $CODE_GENDER)
@@ -59,13 +64,18 @@ str(credit_filtered)
 names(credit_filtered)
 
 # Training validation split -------------------------------------------------------
+# For the training of our model, we decided to use a decision tree. 
+# As for the actual training, we used the favorite seed number, and a 60/40 training/validation split.
+
 set.seed(666)
 train_index <- sample(1:nrow(credit_filtered), 0.6 * nrow(credit_filtered))
 valid_index <- setdiff(1:nrow(credit_filtered), train_index)
-# Input index to the variables
+
+# Inputting index to the variables
 train_df <- credit_filtered[train_index, ]
 valid_df <- credit_filtered[valid_index, ]
-# Count data of train and valid
+
+# Counting data of training and validiation
 nrow(train_df)
 nrow(valid_df)
 
@@ -82,19 +92,29 @@ class_tr <- rpart(TARGET ~.,
 prp(class_tr, cex = 0.8, tweak = 1)
 
 # Confusion matrix
-# Evaluate training set prediction
+
+# Evaluating training set prediction
 classTree_train_pred <- predict(class_tr, train_df, type = "class")
 confusionMatrix(classTree_train_pred , train_df$TARGET,
                 positive = "High-risk")
-# Summary ------------------------------------------------------------
-# Accuracy is 0.6745, which is predicted that 10857 is Low price, with the percentage that
-# 10063 is 100% having a low risk and 794 is not 100% having a low risk
-# On the other hand, which is predicted that 4710 is high price, with the percentage that
-# 437 is 100% having a high price and 4273 is not 100% having a high price.
 
+# Evaluating validation set prediction
 classTree_valid_pred <- predict(class_tr, valid_df, type = "class")
 confusionMatrix(classTree_valid_pred , valid_df$TARGET,
                 positive = "High-risk")
+
+# ROC Curve
+ROSE::roc.curve(valid_df$TARGET, classTree_valid_pred)
+
+# Evaluation ------------------------------------------------------------
+# Accuracy is 0.6745, which means the model is 67.45% accurate at predicting in general 
+# Sensitivity is 0.355, which means the model is only 35.5% accurate when predicting the positive class (high-risk)
+# Specificity is 0.70194, which means the model is 70.19% accurate when predicting the negative class (low-risk)
+# Results from the validation matrix is very similar to the training, so we can see that there is little to no over-fitting here.
+# This means that our model will be fairly accurate across different sets of data.
+
+
+# Predicting the new customers ---------------------------------------------
 
 # Create new customers profile
 ID_250539 <- data.frame(NAME_CONTRACT_TYPE= "Cash loans",
@@ -128,8 +148,15 @@ ID_405632 <- data.frame(NAME_CONTRACT_TYPE= "Cash loans",
                         FLAG_WORK_PHONE = 0, FLAG_EMAIL = 0,CNT_FAM_MEMBERS = 2, REGION_RATING_CLIENT =2,
                         LIVE_REGION_NOT_WORK_REGION = 0, REG_CITY_NOT_WORK_CITY =1, 
                         AMT_REQ_CREDIT_BUREAU_YEAR = 3)
-# ID_250539 :
-## Convert into factor
+
+ID_405632 <- data.frame(NAME_CONTRACT_TYPE= "Cash loans",
+                        CODE_GENDER = "M",FLAG_OWN_CAR="Y",FLAG_OWN_REALTY="Y",
+                        CNT_CHILDREN = 0,AMT_INCOME_TOTAL = 247500, AMT_GOODS_PRICE = 531000,
+                        FLAG_WORK_PHONE = 0, FLAG_EMAIL = 0,CNT_FAM_MEMBERS = 2, REGION_RATING_CLIENT =2,
+                        LIVE_REGION_NOT_WORK_REGION = 0, REG_CITY_NOT_WORK_CITY =1, 
+                        AMT_REQ_CREDIT_BUREAU_YEAR = 3)
+
+## Converting all variables into factor
 ID_250539 $NAME_CONTRACT_TYPE  <- as.factor(ID_250539$NAME_CONTRACT_TYPE)
 ID_250539 $CODE_GENDER <- as.factor(ID_250539 $CODE_GENDER)
 ID_250539 $FLAG_OWN_CAR <- as.factor(ID_250539 $FLAG_OWN_CAR)
@@ -140,12 +167,7 @@ ID_250539 $REGION_RATING_CLIENT <- as.factor(ID_250539 $REGION_RATING_CLIENT)
 ID_250539 $LIVE_REGION_NOT_WORK_REGION <- as.factor(ID_250539 $LIVE_REGION_NOT_WORK_REGION)
 ID_250539 $REG_CITY_NOT_WORK_CITY <- as.factor(ID_250539 $REG_CITY_NOT_WORK_CITY)
 str(ID_250539)
-## Predict 
 
-ID_250539_pred <- predict(class_tr, newdata = ID_250539)
-ID_250539_pred
-
-# ID_442361
 ID_442361 $NAME_CONTRACT_TYPE  <- as.factor(ID_442361$NAME_CONTRACT_TYPE)
 ID_442361 $CODE_GENDER <- as.factor(ID_442361 $CODE_GENDER)
 ID_442361 $FLAG_OWN_CAR <- as.factor(ID_442361 $FLAG_OWN_CAR)
@@ -155,12 +177,7 @@ ID_442361 $FLAG_EMAIL <- as.factor(ID_442361 $FLAG_EMAIL)
 ID_442361 $REGION_RATING_CLIENT <- as.factor(ID_442361 $REGION_RATING_CLIENT)
 ID_442361 $LIVE_REGION_NOT_WORK_REGION <- as.factor(ID_442361 $LIVE_REGION_NOT_WORK_REGION)
 ID_442361 $REG_CITY_NOT_WORK_CITY <- as.factor(ID_442361 $REG_CITY_NOT_WORK_CITY)
-## Predict 
 
-ID_442361_pred <- predict(class_tr, newdata = ID_442361)
-ID_442361_pred
-
-# ID_280650 :
 ID_280650 $NAME_CONTRACT_TYPE  <- as.factor(ID_280650$NAME_CONTRACT_TYPE)
 ID_280650 $CODE_GENDER <- as.factor(ID_280650 $CODE_GENDER)
 ID_280650 $FLAG_OWN_CAR <- as.factor(ID_280650 $FLAG_OWN_CAR)
@@ -170,13 +187,7 @@ ID_280650 $FLAG_EMAIL <- as.factor(ID_280650 $FLAG_EMAIL)
 ID_280650 $REGION_RATING_CLIENT <- as.factor(ID_280650 $REGION_RATING_CLIENT)
 ID_280650 $LIVE_REGION_NOT_WORK_REGION <- as.factor(ID_280650 $LIVE_REGION_NOT_WORK_REGION)
 ID_280650 $REG_CITY_NOT_WORK_CITY <- as.factor(ID_280650 $REG_CITY_NOT_WORK_CITY)
-## Predict 
 
-ID_280650_pred <- predict(class_tr, newdata = ID_280650)
-ID_280650_pred
-
-# ID_420851 :
-## Convert into factor
 ID_420851 $NAME_CONTRACT_TYPE  <- as.factor(ID_420851$NAME_CONTRACT_TYPE)
 ID_420851 $CODE_GENDER <- as.factor(ID_420851 $CODE_GENDER)
 ID_420851 $FLAG_OWN_CAR <- as.factor(ID_420851 $FLAG_OWN_CAR)
@@ -188,20 +199,6 @@ ID_420851 $LIVE_REGION_NOT_WORK_REGION <- as.factor(ID_420851 $LIVE_REGION_NOT_W
 ID_420851 $REG_CITY_NOT_WORK_CITY <- as.factor(ID_420851 $REG_CITY_NOT_WORK_CITY)
 str(ID_420851)
 
-## Predict 
-
-ID_420851_pred <- predict(class_tr, newdata = ID_420851)
-ID_420851_pred
-
-# ID_405632 :
-ID_405632 <- data.frame(NAME_CONTRACT_TYPE= "Cash loans",
-                        CODE_GENDER = "M",FLAG_OWN_CAR="Y",FLAG_OWN_REALTY="Y",
-                        CNT_CHILDREN = 0,AMT_INCOME_TOTAL = 247500, AMT_GOODS_PRICE = 531000,
-                        FLAG_WORK_PHONE = 0, FLAG_EMAIL = 0,CNT_FAM_MEMBERS = 2, REGION_RATING_CLIENT =2,
-                        LIVE_REGION_NOT_WORK_REGION = 0, REG_CITY_NOT_WORK_CITY =1, 
-                        AMT_REQ_CREDIT_BUREAU_YEAR = 3)
-
-## Convert into factor
 ID_405632$NAME_CONTRACT_TYPE  <- as.factor(ID_405632$NAME_CONTRACT_TYPE)
 ID_405632 $CODE_GENDER <- as.factor(ID_405632 $CODE_GENDER)
 ID_405632 $FLAG_OWN_CAR <- as.factor(ID_405632 $FLAG_OWN_CAR)
@@ -213,13 +210,26 @@ ID_405632 $LIVE_REGION_NOT_WORK_REGION <- as.factor(ID_405632 $LIVE_REGION_NOT_W
 ID_405632 $REG_CITY_NOT_WORK_CITY <- as.factor(ID_405632 $REG_CITY_NOT_WORK_CITY)
 str(ID_405632)
 
-## Predict
+# Predicting the new customers
+
+ID_250539_pred <- predict(class_tr, newdata = ID_250539)
+ID_250539_pred
+
+ID_442361_pred <- predict(class_tr, newdata = ID_442361)
+ID_442361_pred
+
+ID_280650_pred <- predict(class_tr, newdata = ID_280650)
+ID_280650_pred
+
+ID_420851_pred <- predict(class_tr, newdata = ID_420851)
+ID_420851_pred
+
 ID_405632_pred <- predict(class_tr, newdata = ID_405632)
 ID_405632_pred
 
-# Explanation of set up and variable selection, results, interpretation, model evaluation
-
 # Discussion ------------------------------------------------------------
+# Our final model was fairly accurate, as both the training and validation accuracy was fairly decent (67%)
+
 
 # Final Recommendation --------------------------------------------------
 
